bd194875ddeba847a974e3a05da29cc89d326fd3
53hyun <osh65411@naver.com>
2025-04-29 22:50:17 -0900

    1234

---
+++ hello1.py
@@ -0,0 +1,28 @@
+import RPi.GPIO as GPIO
+
+from time import sleep
+
+LED = [11, 13, 15]
+Switch = 10
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(Switch, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(LED, GPIO.OUT)
+
+try:
+    while True:
+        
+        if GPIO.input(Switch) == GPIO.HIGH:
+            
+            GPIO.output(LED,GPIO.HIGH)
+            print('LED ON')
+            sleep(1)
+        
+        else:
+            
+            GPIO.output(LED,GPIO.LOW)
+            print('LED Off')
+            sleep(1)
+finally:
+    
+    GPIO.cleanup()
